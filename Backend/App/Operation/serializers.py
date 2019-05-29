e1bd4082d85656e6856e2317ba2b1b7b6fc32fc5
JayDhi <jamesfranz1998@gmail.com>
2019-05-29 20:54:21 +0800

    1.修复OperationSerializer在迭代子项时筛选条件丢失的问题; 2.自定义OperationSerializer的创建与验证

--- Backend/App/Operation/serializers.py
+++ Backend/App/Operation/serializers.py
@@ -16,10 +16,31 @@ class OperationSerializer(serializers.ModelSerializer):
             return reverse("%s:%s" % (obj.app_name, obj.view_name))
     def get_sub_operations(self, obj):
         try:
-            return [OperationSerializer(i).data for i in Operation.objects.filter(parent_operation_id=obj.operation_id)]
+            # 要将self.context传入迭代列表, 否则迭代停止
+            return [OperationSerializer(i, context=self.context).data 
+                    for i in Operation.objects.filter(parent_operation_id=obj.operation_id, 
+                                                     role_belong_to__contains=str(self.context["role"]))]
         except:
             traceback.print_exc()
             return None
+    def create(self, data):
+        data["operation_id"] = Operation.objects.count()
+        data["parent_operation_id"] = Operation.objects.filter(name=self.context["parent_operation"]).values('operation_id')[0]["operation_id"]
+        print(data)
+        return Operation.objects.create(**data)
+    # 自定义validate: 传入parent_operation_name, data["parent_operation_name"]
+    # 但会引发KeyError: 'parent_operation_name'
+    # data --> OrderedDict{} 并不含有parent_operation_name, 可能是在最初的过程中被清洗
+
+    def validate(self, data):
+        if self.context["parent_operation"] == "":
+            return data
+        else:
+            try:
+                Operation.objects.get(name=self.context["parent_operation"])
+                return data
+            except:
+                raise serializers.ValidationError("Parent Operation DOES NOT exist!!")
     class Meta:
         model = Operation
         fields = ('name', 'url', 'description', 'sub_operations')
--- Backend/App/Operation/urls.py
+++ Backend/App/Operation/urls.py
@@ -1,4 +1,10 @@
 # App/Operation/urls.py
 # import from framework
-
+from django.urls import path
 # import from project
+from App.Operation import views
+
+app_name="Operation"
+urlpatterns = [
+    path('get_user_menu/', views.get_user_menu, name='get_user_menu'),
+]
