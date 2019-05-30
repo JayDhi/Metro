065c5dd5b0e891be0235b68f6ae76ce48e9995c0
JayDhi <jamesfranz1998@gmail.com>
2019-05-30 21:38:21 +0800

    Serializer优化(1):1.User(在用户注册完后返回Tokens&Menu); 2.Operation

--- Backend/App/Operation/views.py
+++ Backend/App/Operation/views.py
@@ -6,16 +6,19 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 # import from project
 from .models import Operation
-from .serializers import OperationSerializer, EditOperation
+from .serializers import ShowOperation, EditOperation
+
+# -[x] Create
+# -[x] Retrive
+# -[x] Update
+# -[ ] Delete
 
 # TO-DO: 用装饰器实现菜单过滤以及角色验证
 @api_view(['GET'])
 @permission_classes((IsAuthenticated,))
-def get_user_menu(request):
-    print(request.user.role)
-    top_operations = Operation.objects.filter(role_belong_to__contains=str(request.user.role), parent_operation=0)
-    menu = OperationSerializer(top_operations, context={"role": str(request.user.role)}, many=True)
-    print(menu.data)
+def get_menu(request):
+    top_operations = Operation.objects.filter(role__contains=str(request.user.role), parent_operation=0)
+    menu = ShowOperation(top_operations, context={"role": str(request.user.role)}, many=True)
     return JsonResponse(data=menu.data, safe=False)
 
 # 添加菜单根节点的时候需要将其parent_operation设为NullOperation # fix: 处理MySQL中Null字段
@@ -23,29 +26,11 @@ def get_user_menu(request):
 # 由于数据库设计的缘故需要手动生成operation_id(主键),
 # 在有0号对象(NullOperation)的情况下operation_id=Operation.objects.count()
 # validate()检测是否存在父节点对象
-@api_view(['POST'])
-@permission_classes((IsAdminUser,))
-# input json format {"parent_operation", "name", }
-def add_operation(request):
-    serializer = OperationSerializer(data=request.data, context={"parent_operation": request.data["parent_operation"]})
-    pass
 
+# 修改菜单栏: 重链父子节点、修改角色过滤
+# 需要管理员权限
 @api_view(['POST'])
-@permission_classes((IsAdminUser,))
-def link(request):
-    pass
-
-@api_view(['POST'])
-def update_role(request):
-    # data = ["role": [], "operation": ""]
-    """name = request.data.pop("operation")
-    operation = Operation.objects.get(name=name)
-    serializer = OperationSerializer(operation, data={"name": name}, context={"role_belong_to": request.data.pop("role")})
-    if serializer.is_valid():
-        serializer.save()
-        return JsonResponse(data=serializer.data, safe=False)
-    else:
-        return JsonResponse(serializer.errors)"""
+def edit_menu(request):
     try:
         operation = Operation.objects.get(operation_id=request.data["operation_id"])
         slzr = EditOperation(instance=operation, data=request.data)
--- Backend/App/User/views.py
+++ Backend/App/User/views.py
@@ -6,17 +6,25 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 # import from project
-from App.Operation.models import Operation
-from App.Operation.serializers import OperationSerializer
 from .models import User
 from .serializers import UserSerializer, AdminUser, RegisterUser
 
-@api_view(['GET'])
-@permission_classes((IsAuthenticated,))
-def test(request):
-    content = {"username": request.user.username, "role": request.user.role, "role_category": request.user.role_category}
-    return JsonResponse(content)
+# -[x] Create
+# -[x] Retrive
+# -[x] Update
+# -[ ] Delete
 
+# 注册
+@api_view(['POST'])
+def register(request):
+    user_slzr = RegisterUser(data=request.data)
+    if user_slzr.is_valid():
+        user_slzr.save()
+        return JsonResponse(data=user_slzr.data, safe=False)
+    else:
+        return JsonResponse(data=user_slzr.errors)
+
+# 需要用户登录
 @api_view(['GET'])
 @permission_classes((IsAuthenticated,))
 def view_profile(request):
@@ -24,7 +32,7 @@ def view_profile(request):
     user_slzr = UserSerializer(instance=user)
     return JsonResponse(data=user_slzr.data)
 
-# !Admin Required #
+# 需要管理员权限
 @api_view(['GET', 'POST'])
 @permission_classes((IsAdminUser,))
 def admin_user(request):
@@ -45,20 +53,3 @@ def admin_user(request):
             return JsonResponse(data=user_slzr.data, safe=False)
         else:
             return JsonResponse(data=user_slzr.errors, safe=False)
-        
-
-# register
-@api_view(['POST'])
-def register(request):
-    user_slzr = RegisterUser(data=request.data)
-    if user_slzr.is_valid():
-        user_slzr.save()
-        role = user_slzr.data["role"]
-        root_operation = Operation.objects.filter(parent_operation_id = 0, role_belong_to__contains=str(role))
-        interface = {}
-        interface["UserInfo"] = user_slzr.data
-        menu = OperationSerializer(root_operation, many=True, context={"role": role})
-        interface["Menu"] = menu.data
-        return JsonResponse(data=interface, safe=False)
-    else:
-        return JsonResponse(data=user_slzr.errors)
