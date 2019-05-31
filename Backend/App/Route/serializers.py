8212e38f0d3f9de1e570049b093074e6fb1b7d25
JayDhi <jamesfranz1998@gmail.com>
2019-05-31 20:13:33 +0800

    Refactor(Route.serializers): 解耦功能
    - Refactor(Route.serializers):
        - 移除RouteSerailizers的'创建'功能
        - 用EditRoute实现'编辑'功能

--- Backend/App/Route/serializers.py
+++ Backend/App/Route/serializers.py
@@ -2,11 +2,78 @@
 # import from framework
 from rest_framework import serializers
 # import from project
-from App.Combination.serializers import RouteXStationSerializer
 from .models import Route
+from App.Combination.models import RouteXStation
+from App.Combination.serializers import RouteXStationSerializer
 
-class RouteSerializer(serializers.ModelSerializer):
+# 用于显示站点
+class ShowRoute(serializers.ModelSerializer):
     stations = RouteXStationSerializer(source='routexstation_set', required=False, many=True)
     class Meta:
         model = Route
-        fields = ('route_id', 'stations')
+        fields = ('route_name', 'route_id', 'stations')
+
+# 用于编辑站点
+# {"route":
+#  "relationship": {
+#       station1:{"station_name": , "seq":},
+#       station2:{"station_name": , "seq":}}}
+class EditRoute(serializers.ModelSerializer):
+    def create(self, data):
+        route = Route.objects.create(route_id=Route.objects.count()+1, route_name=data.pop("route_name"))
+        for k in data:
+            try:
+                combination = RouteXStationSerializer(data={"route": route.route_id, "seq": data[k]["seq"], "station": data[k]["station_id"]})
+                if combination.is_valid():
+                    combination.save()
+                else:
+                    raise Exception("Failed to combine route & station")
+            except KeyError:
+                pass
+        return route
+    def update(self, instance, data):
+        try:
+            # 目前仅支持更改站点名称
+            # 为了避免对附属站点的遍历过于麻烦，将data结构设计为{"route_id", "station_1", ...}
+            # 而不是{"route_id", "stations":{"station1", ...}}
+            # 而这种结构在遍历站点信息的时候应当保证字典是{"station1", ....}
+            # 所以现将route_id取出保存在另一个变量中
+            route_name = data.pop("route_name")
+            instance.route_name = route_name
+            for k in data:
+                print(k)
+                ex_relation = RouteXStation.objects.filter(route_id=instance.route_id, station_id=data[k]["station_id"])
+                if ex_relation:
+                    ex_relation.delete()
+                new_relation = RouteXStationSerializer(data={"route": instance.route_id, "seq": data[k]["seq"], "station": data[k]["station_id"]})
+                if new_relation.is_valid():
+                    new_relation.save()
+                    print(new_relation.data)
+                else:
+                    raise Exception("Failed to combine route & station")
+        except Exception:
+            raise Exception
+        instance.save()
+        return instance
+    def validate(self, data):
+        try:
+            for k in self.context:
+                print(k)
+                station = RouteXStation.objects.filter(station_id=self.context[k]["station_id"])
+                if station:
+                    try:
+                        station.get(seq=self.context[k]["seq"])
+                    except RouteXStation.DoesNotExist:
+                        pass
+                    finally:
+                        data[k] = {}
+                        data[k]["station_id"] = self.context[k]["station_id"]
+                        data[k]["seq"] = self.context[k]["seq"]
+                else:
+                    raise serializers.ValidationError("Station of station_id '%s' DOES NOT exist" % self.context[k]["station_id"])
+            return data
+        except KeyError:
+            return data
+    class Meta:
+        model = Route
+        fields = ('route_name',)
--- Backend/App/Route/views.py
+++ Backend/App/Route/views.py
@@ -5,19 +5,35 @@ from django.http import JsonResponse
 from rest_framework.decorators import api_view
 # import from project
 from .models import Route
-from .serializers import RouteSerializer
+from .serializers import ShowRoute, EditRoute
 
 @api_view(['GET'])
-def route_list(request):
-    route_set = Route.objects.all()
-    route_slzr = RouteSerializer(instance=route_set, many=True)
-    return JsonResponse(data=route_slzr.data, safe=False)
-
-@api_view(['POST'])
-def route_create(request):
-    route_slzr = RouteSerializer(data=request.data)
-    if route_slzr.is_valid():
-        route_slzr.save()
-        return JsonResponse(route_slzr.data, safe=False)
+def get_route(request):
+    if "route" in request.data:
+        slzr = ShowRoute(Route.objects.filter(
+                                    route_id__in=request.data["route"]), 
+                                        many=True)
     else:
-        return JsonResponse(route_slzr.errors)
+        slzr = ShowRoute(Route.objects.all(), many=True)
+    return JsonResponse(data=slzr.data, safe=False)
+
+
+# input format
+# {"route_info": {"route_name"}, "relationship":{}}
+@api_view(['POST'])
+def edit_route(request):
+    data = request.data["route_info"]
+    context = request.data["relationship"]
+    try:
+        route = Route.objects.get(route_name=data["route_name"])
+    except KeyError:
+        return JsonResponse(data={"errors": "Check Json Parameter"})
+    except Route.DoesNotExist:
+        slzr = EditRoute(data=data, context=context)
+    else:
+        slzr = EditRoute(route, data=data, context=context)
+    if slzr.is_valid():
+        slzr.save()
+        return JsonResponse(data=slzr.data, safe=False)
+    else:
+        return JsonResponse(slzr.errors)
