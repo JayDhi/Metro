b453d1c1d7e911312895bf1c23a1724d8c03c662
JayDhi <jamesfranz1998@gmail.com>
2019-06-01 15:31:59 +0800

    错误修复(1):1.Post.models; 2.User.views

--- Backend/App/Post/models.py
+++ Backend/App/Post/models.py
@@ -4,8 +4,8 @@ from django.db import models
 # import from project
 
 class Post(models.Model):
-    post_title = models.CharField(max_length=20, required=True)
-    post_body = models.CharField(max_length=200, required=True)
+    post_title = models.CharField(max_length=20)
+    post_body = models.CharField(max_length=200)
     post_linkn = models.URLField(max_length=250)
     def __str__(self):
         return self.post_title
