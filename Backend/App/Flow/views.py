45f8c7d6fa7aab5caf8bf91dd81c70dffc2fd1cf
JayDhi <jamesfranz1998@gmail.com>
2019-06-12 19:15:53 +0800

    将Flow/show_flow/的请求方式改成POST以支持更广泛的请求格式

--- Backend/App/Flow/views.py
+++ Backend/App/Flow/views.py
@@ -6,10 +6,10 @@ from rest_framework.decorators import api_view
 # import from project
 from .models import get_flow_data
 
-@api_view(['GET'])
+@api_view(['POST'])
 def show_flow(request):
-    data = get_flow_data(year=request.query_params["year"],
-                         month=request.query_params["month"],
-                         dates=request.query_params["dates"], 
-                         stations=request.query_params["stations"])
+    data = get_flow_data(year=request.data["year"],
+                         month=request.data["month"],
+                         dates=request.data["dates"], 
+                         stations=request.data["stations"])
     return JsonResponse(data=data, safe=False)
